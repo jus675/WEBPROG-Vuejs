960243890cdadb5056417f1f05a271b4629c937c
Justine Denise Hernandez <114111385+jus675@users.noreply.github.com>
2025-01-27 14:27:13 +0800

    added personal profile components

---
+++ firstsfc/src/components/PersonalProfile.vue
@@ -0,0 +1,90 @@
+<template>
+    <div class="container">
+        <header>
+            <h1> Personal Profile Web Page </h1>
+        </header>
+        <main>
+            <section class="main-content">
+                <h2> About Me </h2>
+                    <img src="Justine.png" alt="Justine" style="width:250px;height:260px">
+                    <p>I am Justine Denise Hernandez, a 3rd year Computer Science student from SS221, currently studying at Asia Pacific College.</p>
+                    <p>The field of Computer Science piqued my interest as I explored interesting software and websites during the pandemic.</p>
+                <h2>Education</h2>
+                    <table>
+                        <tr>
+                            <th>Year Level</th>
+                            <th>School</th>
+                        </tr>
+                        <tr>
+                            <td>2022 - 2026</td>
+                            <td>Asia Pacific College</td>
+                        </tr>
+                        <tr>
+                            <td>2016 - 2022</td>
+                            <td>Pasay City National Science High School</td>
+                        </tr>
+                        <tr>
+                            <td>2010 - 2016</td>
+                            <td>St. Mary's Academy Pasay City</td>
+                        </tr>
+                        <tr>
+                            <td>2008 - 2010</td>
+                            <td>St. Therese CJHF School</td>
+                        </tr>
+                        <tr>
+                            <td>2007 - 2008</td>
+                            <td>R&O Academy</td>
+                        </tr>
+                    </table>
+                
+                <h2>Achievements</h2>
+                <ul>
+                    <li>Honor Student (from Elem to College)</li>
+                    <li>APC-JPCS Director of Operations (2024-)</li>
+                    <li>Animation Society - Secretary (2022-2022)</li>
+                </ul>
+
+                <h2>Course</h2>
+                <p>Bachelor of Science in Computer Science with specialization in Software Systems</p>
+
+                <h2>I.T. Experience</h2>
+                <ul>
+                    <li>Python</li>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>Javascript</li>
+                </ul>
+
+                <h2>Hobbies & Interests</h2>
+                <ul>
+                    <li>I love to watch movies, series, k-drama, anime and video games <strong style=color:pink>#Cinephile </strong> &#10024;</li>
+                    <li>I love going to movie theatres for film festivals &#127909;</li>
+                    <li>I love listening to music like Pop, K-Pop, OPM, Rock, Shoegaze &#9835;</li>
+                    <li>I love walking and riding the bicycle &#128690;</li>
+                    <li>I love playing badminton &#127992;</li>
+                    <li>I love meditating and journaling &#128211;</li>
+                </ul>
+
+                <h2>Goals in Life</h2>
+                <ul>
+                    <li>To be a software developer</li>
+                    <li>To fulfill true peace and happiness</li>
+                    <li>To achieve financial stability</li>
+                    <li>To strive for excellence</li>
+                    <li style="color:lightblue"><s><i>To have a homelab server perhaps</i></s></li>
+                </ul>
+
+                <h2>Picture Gallery</h2>
+                <img src="Picture Gallery - 1.jpg" alt="Picture Gallery - 1" style="width:300px;height:400px;">
+                <img src="Picture Gallery - 2.jpg" alt="Picture Gallery - 2 " style="width:550px;height:400px;">
+                <img src="Picture Gallery - 3.jpg" alt="Picture Gallery - 3" style="width:550px;height:400px;">
+                <img src="Picture Gallery - 5.jpg" alt="Picture Gallery - 5" style="width:600px;height:400px;">
+                <img src="Picture Gallery - 4.jpg" alt="Picture Gallery - 4" style="width:300px;height:500px;">
+                <img src="Picture Gallery - 6.jpg" alt="Picture Gallery - 6" style="width:4350px;height:500px;">
+                    </div>
+            </section>
+        </main>
+    </template>
+    
+    <script></script>
+    <style></style>
