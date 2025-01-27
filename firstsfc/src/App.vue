a4fea68e0876c1ad87cc50e297ad8b7c6b21e14b
Justine Denise Hernandez <114111385+jus675@users.noreply.github.com>
2025-01-27 14:50:13 +0800

    test

--- firstsfc/src/App.vue
+++ firstsfc/src/App.vue
@@ -1,12 +1,8 @@
 <template>
     <h1>Food</h1>
     <personal-profile/> <br>
-  <food-item/>
+  <food-item/> 
   <food-item2/> 
   <food-item/>  
-  <food-item2/> 
 </template>
 
-<script></script>
-
-<style> </style>
